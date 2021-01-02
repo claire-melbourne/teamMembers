b2eae3c07a26f26dba98893c7b834dec4cbd61f5
Claire Melbourne <claire.melbourne@gmail.com>
2021-01-02 17:04:36 -0600

    Change button text to 'Join the team!'

--- src/components/TeamMember/TeamMember.js
+++ src/components/TeamMember/TeamMember.js
@@ -20,7 +20,7 @@ class TeamMember extends React.PureComponent {
 
   handleClick() {
     this.props.toggleFormView();
-    alert('form button clicked');
+    //alert('form button clicked');
   }
 
   render() {
@@ -38,7 +38,7 @@ class TeamMember extends React.PureComponent {
           <h1 className="name">{this.props.name}</h1>
         </header>
         <div className="body">{this.props.story}</div>
-        <button className="button" onClick= {()=> this.handleClick()}>{this.props.id ===undefined ? null : 'button'}</button>
+        <button className="button" onClick= {()=> this.handleClick()}>{this.props.id ===undefined ? null : 'Join the team!'}</button>
         <footer style={{ backgroundColor: this.props.favoriteColor }}>
           <div className="full-width-flex-box">
             <div className="one-third-flex-box stat">9.0</div>
