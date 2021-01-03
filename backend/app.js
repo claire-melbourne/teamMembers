5dc9d09cb6f02fb72c2ff6f07ac903764bf95d56
Claire Melbourne <claire.melbourne@gmail.com>
2021-01-02 18:11:15 -0600

    Implement post api controller for new member

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,28 @@ const express = require('express');
 const { TeamMember } = require('./model');
 
 const app = express();
+app.use(express.json());
 
 app.get('/team', async (req, res, next) => {
   const team = await TeamMember.findAll();
   return res.json(team);
 });
+
+app.post('/newMember', async (req, res, next) => {
+  console.log('received data: ', req.body)
+  try {
+    const newMember = await TeamMember.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      title: req.body.title,
+      story: req.body.story,
+      favoriteColor: req.body.favoriteColor,
+      photoUrl: req.body.photoUrl
+    })
+    res.json(newMember)
+  } catch (error) {
+    console.log(error)
+  }
+})
 
 module.exports = app;
